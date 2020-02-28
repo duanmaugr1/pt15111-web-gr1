591f1e262a93c323499d67d1f50569f86bb295cd
Hoangnhph09381 <57469906+Hoangnhph09381@users.noreply.github.com>
2020-02-28 14:11:13 +0700

    Initial commit

---
+++ _share/stype.php
@@ -0,0 +1,28 @@
+<!-- Bootstrap -->
+<link href="<?= THEME_ASSET_URL ?>dist/css/bootstrap.css" rel="stylesheet" media="screen">
+<link href="<?= THEME_ASSET_URL ?>assets/css/custom.css" rel="stylesheet" media="screen">
+
+<!-- Carousel -->
+<link href="<?= THEME_ASSET_URL ?>examples/carousel/carousel.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+    <script src="<?= THEME_ASSET_URL ?>assets/js/html5shiv.js"></script>
+    <script src="<?= THEME_ASSET_URL ?>assets/js/respond.min.js"></script>
+    <![endif]-->
+
+<!-- Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,300,300italic' rel='stylesheet' type='text/css'>
+<!-- Font-Awesome -->
+<link rel="stylesheet" type="text/css" href="<?= THEME_ASSET_URL ?>assets/css/font-awesome.css" media="screen" />
+<!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="<?= THEME_ASSET_URL ?>assets/css/font-awesome-ie7.css" media="screen" /><![endif]-->
+
+<!-- REVOLUTION BANNER CSS SETTINGS -->
+<link rel="stylesheet" type="text/css" href="<?= THEME_ASSET_URL ?>css/fullscreen.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="<?= THEME_ASSET_URL ?>rs-plugin/css/settings.css" media="screen" />
+
+<!-- Picker UI-->
+<link rel="stylesheet" href="<?= THEME_ASSET_URL ?>assets/css/jquery-ui.css" />
+
+<!-- jQuery -->
+<script src="<?= THEME_ASSET_URL ?>assets/js/jquery.v2.0.3.js"></script>
